47bb05e129b10e900fc54fb3292a18e1d60b660a
SimChoonYee <cysim93@gmail.com>
2019-07-02 01:16:19 +0800

    FwClass init

--- pmod/pcusfw.py
+++ pmod/pcusfw.py
@@ -1,4 +1,3 @@
-
 from pgeneric.pimport_module import *
 
 
@@ -6,20 +5,29 @@ class FwClass:
     def init(self, *args, **kwargs):
         logging.info('INIT:%s', self.__class__.__name__)
 
+        # Results
         self.fw_name = None
         self.fw_version = None
 
         self.fm = FileMethodClass()
         self.fm.init(*args, **kwargs)
         # FileMethodClass attributes
-        self.fw_path = None
-        self.fw_dir = None
-        self.fw_search_dir = None
-        self.fw_version = None
 
+        # Data
+        self.fw = {'info': None, 'zip': None}
+
+        data = {'fw_path': None,
+                'fw_dir': None,
+                'fw_search_dir': None}
+
+        for key in self.fw:
+            self.fw[key] = data
+
+        logging.debug('STORE self.fw info & zip %s', self.fw)
+
+    # def extract_file(self):
+    #     pass
 
 if __name__ == "__main__":
     cusfw = FwClass()
-
-
-
+    cusfw.init()
